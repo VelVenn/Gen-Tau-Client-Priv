a32c6742b8940a9ee04aebb5ccd9fcb731fb08e1
Loris Rosa <162586864+VelVenn@users.noreply.github.com>
2026-04-16 19:58:22 +0800

    Added support for type and undefined behavior script, forcing clang when using *san

--- cmake/TCompileOptions.cmake
+++ cmake/TCompileOptions.cmake
@@ -1,22 +1,56 @@
-# =========================== ENABLE ADDRESS SANITIZER ===========================
-option(GEN_TAU_USE_ASAN "Enable address sanitizer for all gen-τ targets (ONLY FOR GNU/LLVM compilers)" OFF)
+# =========================== ENABLE SANITIZER ===========================
+option(GEN_TAU_USE_ASAN "Enable address sanitizer for all gen-τ targets" OFF)
+option(GEN_TAU_USE_TYPE_SAN "Enable type sanitizer for all gen-τ targets" OFF)
+option(GEN_TAU_SAN_OPT_LEVEL "Optimization level for address sanitizer builds (default: O0)" O0)
+
+if(GEN_TAU_USE_ASAN AND GEN_TAU_USE_TYPE_SAN)
+  message(FATAL_ERROR "!! Cannot enable both Address Sanitizer and Type Sanitizer at the same time !!")
+endif()
+
+if(GEN_TAU_USE_ASAN OR GEN_TAU_USE_TYPE_SAN)
+  set(CMAKE_C_COMPILER clang CACHE STRING "" FORCE)
+  set(CMAKE_CXX_COMPILER clang++ CACHE STRING "" FORCE)
+
+  set(LLVM_ENABLE_PROJECTS "clang" CACHE STRING "" FORCE)
+  set(LLVM_ENABLE_RUNTIMES "compiler-rt" CACHE STRING "" FORCE)
+
+  message(STATUS "-> Sanitizer enabled, forcing Clang as the compiler, optimazation level: ${GEN_TAU_SAN_OPT_LEVEL}")
+
+  add_compile_options(
+    -fsanitize-ignorelist=${CMAKE_SOURCE_DIR}/scripts/.san-ignore
+    -${GEN_TAU_SAN_OPT_LEVEL}
+    -g
+  )
+endif()
 
 if(GEN_TAU_USE_ASAN)
-  if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-    message(STATUS "-> Address Sanitizer enabled")
-    add_compile_options(
-      -fsanitize=address 
-      -fno-omit-frame-pointer
-      -fno-optimize-sibling-calls
-      -fsanitize-address-use-after-scope
-      -fsanitize-recover=address
-      -O0
-    )
-    add_link_options(-fsanitize=address)
+  message(STATUS "-> Address Sanitizer enabled")  
+  add_compile_options(
+    -fsanitize=address,undefined
+    -fno-omit-frame-pointer
+    -fno-optimize-sibling-calls
+    -fsanitize-address-use-after-scope
+    -fsanitize-recover=address
+  )
+  add_link_options(-fsanitize=address,undefined)
 
-    if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
-      message(WARNING "! You are using Adrress Sanitizer in non-Debug mode, which may lead to unexpected behavior and performance issues. !")
-    endif()
+  if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
+    message(WARNING "! You are using Address Sanitizer in non-Debug mode, which may lead to unexpected behavior and performance issues. !")
   endif()
 endif()
-# =========================== ENABLE ADDRESS SANITIZER ===========================
+
+if(GEN_TAU_USE_TYPE_SAN)
+  message(STATUS "-> Type Sanitizer enabled")
+  message(STATUS "-> Please note that if '-fno-strict-aliasing' is set, Type Sanitizer will not work")
+  
+  set(CMAKE_BUILD_TYPE Release CACHE STRING "" FORCE)
+  message(STATUS "-> Forcing build type to Release for Type Sanitizer")
+
+  add_compile_options(
+    -fsanitize=type
+    -fno-omit-frame-pointer
+    -fno-optimize-sibling-calls
+  )
+  add_link_options(-fsanitize=type)
+endif()
+# =========================== ENABLE SANITIZER ===========================
