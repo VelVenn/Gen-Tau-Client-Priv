202688fd856f114d68a7a6c5aecff33dccf5ba5f
Loris <162586864+VelVenn@users.noreply.github.com>
2026-07-26 20:15:27 +0800

    Intergrated game status to hud model

--- app/modules/models/hud/GHudModel.cpp
+++ app/modules/models/hud/GHudModel.cpp
@@ -4,6 +4,7 @@ namespace gentau {
 GHudModel::GHudModel(GMqttAdapter& client, QObject* parent) :
 	QObject(parent),
 	_client(client),
-	_globalStatus(_serializer, _client, this)
+	_globalStatus(_serializer, _client),
+	_gameStatus(_serializer, _client)
 {}
 }  // namespace gentau
--- app/modules/models/hud/inc/models/hud/GHudModel.hpp
+++ app/modules/models/hud/inc/models/hud/GHudModel.hpp
@@ -5,6 +5,7 @@
 
 #include "adapter/mqtt/GMqttAdapter.hpp"
 
+#include "models/game_status/GGameStatusModel.hpp"
 #include "models/global_status/GGlobalStatusModel.hpp"
 
 namespace gentau {
@@ -13,9 +14,11 @@ class GHudModel : public QObject
 	Q_OBJECT
 
 	Q_PROPERTY(gentau::GGlobalStatusModel* globalStatus READ globalStatus CONSTANT FINAL)
+	Q_PROPERTY(gentau::GGameStatusModel* gameStatus READ gameStatus CONSTANT FINAL)
 
   public:
 	GGlobalStatusModel* globalStatus() noexcept { return &_globalStatus; }
+	GGameStatusModel* gameStatus() noexcept { return &_gameStatus; }
 
   private:
 	QProtobufSerializer _serializer;
@@ -23,6 +26,7 @@ class GHudModel : public QObject
 
   private:
 	GGlobalStatusModel _globalStatus;
+	GGameStatusModel   _gameStatus;
 
   public:
 	explicit GHudModel(GMqttAdapter& client, QObject* parent = nullptr);
