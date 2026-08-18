704b48a683ac88dd6e5169043f18dde0e81fa7e2
Loris <162586864+VelVenn@users.noreply.github.com>
2026-08-18 23:04:34 +0800

    Fixed the restoration of frameData on gstbuffer alloc failure

--- core/img_trans/TVidRender.cpp
+++ core/img_trans/TVidRender.cpp
@@ -580,6 +580,8 @@ bool TVidRender::tryPushFrame(TFramePool::FrameData&& frame, TReassemblyPasskey)
 		// 	);
 		// }
 		// Push failure is not a critical error, just skip it and wait for next frame, avoiding log spam.
+	} else {
+		delete frameDataPtr;
 	}
 
 	delete frameDataPtr;
